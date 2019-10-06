85b089894583591235c382d2def8f4a24ce0a344
sarthak sadh <33261776+sarthaksadh01@users.noreply.github.com>
2019-10-07 01:16:45 +0530

    Update functions.php

--- ankisarth/backend/functions.php
+++ ankisarth/backend/functions.php
@@ -2,7 +2,7 @@
 
 function connect_to_database()
 {
-    $link = mysqli_connect("localhost","newuser","password","cryptx");
+    $link = mysqli_connect("us-cdbr-iron-east-05.cleardb.ne","b2935a1eb069ef","aee5efc4","heroku_cd87107b3c6ab97");
     if(mysqli_error($link))
     {
       die("Failed connecting to databse.. Please try again!");
